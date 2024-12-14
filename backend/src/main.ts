3e7005e7715f78fbc2f594832393b618c98b9b64
CaioRdSilva <caiodrs.silva@gmail.com>
2024-12-14 17:14:34 -0300

    setup swagger and import countries module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('DevelopsToday API')
+  .setDescription('API to search countries')
+  .setVersion('1.0')
+  .addTag('countries')
+  .build();
+const documentFactory = () => SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, documentFactory);
+
+
   await app.listen(3000);
 }
 bootstrap();
