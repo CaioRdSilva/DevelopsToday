5bb0c67047ddd0f1f3303455db5f29c7c035ad44
CaioRdSilva <caiodrs.silva@gmail.com>
2024-12-15 02:21:46 -0300

    enable cors and setup swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
 
   const config = new DocumentBuilder()
-  .setTitle('DevelopsToday API')
-  .setDescription('API to search countries')
-  .setVersion('1.0')
-  .addTag('countries')
-  .build();
-const documentFactory = () => SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, documentFactory);
+    .setTitle('DevelopsToday API')
+    .setDescription('API to search countries')
+    .setVersion('1.0')
+    .addTag('countries')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
-
-  await app.listen(3000);
+  await app.listen(3030);
 }
 bootstrap();
